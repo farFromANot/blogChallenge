08a675b5916ed77eee7f79be7adcec893ede4f12
farFromANot <iangibsonc@gmail.com>
2020-02-20 23:10:28 -0600

    log all of the titles from compose section to terminal

--- app.js
+++ app.js
@@ -16,13 +16,15 @@ app.use(bodyParser.urlencoded({extended: true}));
 app.use(express.static("public"));
 
 
-  
+let posts = []
 
 
 app.get("/", function(req, res){
   res.render("home", {
-    startingContent: homeStartingContent
+    startingContent: homeStartingContent,
+    posting: posts
   })
+  
 })
 
 app.get("/about", function(req, res){
@@ -45,9 +47,12 @@ app.get("/compose", function(req, res){
 })
 
 app.post("/compose", function(req, res){
-  let postTitle = req.body.postTitle
-  let postBody = req.body.postBody
-  console.log(postTitle,postBody)
+    const post = {
+      title: req.body.postTitle,
+      content: req.body.postBody
+    }
+    posts.push(post)
+    res.redirect("/")
 })
 
 
